d4d38af7cebb42718fa9b394793cf718d74cf938
Ellee007 <alhassanaliyu83@gmail.com>
2024-03-26 04:41:11 -0700

    file 7&8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
-""" Fetches data from storage engine """
-
+"""Script that starts a Flask web application"""
+from flask import Flask
+from flask import render_template
 from models import storage
 from models.state import State
-from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route("/states_list", strict_slashes=False)
+@app.route('/states_list', strict_slashes=False)
 def states_list():
-    """Displays html page of state in a list format"""
-    states = storage.all(State)
-    return render_template("7-states_list.html", states=states)
+    """Display a HTML with the list of states"""
+    states = storage.all(State).values()
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session"""
+def remove_Alchemy(exception):
+    """Remove the current SQLAlchemy Session"""
     storage.close()
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
